a5b0235a4367747609e45c68ea9a391970516f17
AlexPerrine <perrine.alex6@gmail.com>
2021-06-07 14:12:08 -0500

    Added function to play user defined number of RPS games.

--- rps.js
+++ rps.js
@@ -24,21 +24,43 @@ function playerPlay(){
 //One round
 function playRound(playerSelection,  computerSelection){
     if(playerSelection === computerSelection){
-        return 'Tie game, play again.'
+        return 'tie';
     }
     if(
     (playerSelection == 'rock' && computerSelection == 'scissors') ||
     (playerSelection == 'paper' && computerSelection == 'rock') ||
     (playerSelection == 'scissors' && computerSelection == 'paper')
     )   {
-        return `You win, ${playerSelection} beats ${computerSelection}!`;
+        return 'player';
     }
     if(
     (computerSelection == 'rock' && playerSelection == 'scissors') ||
     (computerSelection == 'paper' && playerSelection == 'rock') ||
-    (playerSelection == 'scissors' && playerSelection == 'paper')
+    (computerSelection == 'scissors' && playerSelection == 'paper')
     )   {
-         return `You lose, ${computerSelection} beats ${playerSelection}!`;
+         return 'computer';
     }
 }
+//Game with a user defined number of rounds, set to 5 as default.
+function game(rounds = 5){
+    const name = window.prompt('Who challenges the computer?');
+    let player = 0;
+    let computer = 0;
 
+    for (let i = 0; i < rounds; i++) {
+        let x = playRound(playerPlay(),computerPlay());
+        if (x == 'player'){
+            player++;
+            console.log(`${name} won this round!`);
+        }
+        else if( x == 'computer'){
+            computer++;
+            console.log('The computer won this round!')
+        }
+        else{
+            i--;
+            console.log('Tie, play again');
+        }
+    };
+    console.log(`${name} won ${player} game(s) and the computer won ${computer} game(s).`)
+}
